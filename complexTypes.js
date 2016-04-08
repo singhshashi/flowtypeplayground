a309e104deba1ef5bb702fdedcc2cd6765c3b8be
Shashi Singh <singh.shashishekhar@gmail.com>
2016-04-09 01:09:40 +0530

    Added case which shows how flow treats structural(aka Object) and nominal(aka class) types differently. See comments in code file

--- complexTypes.js
+++ complexTypes.js
@@ -15,10 +15,30 @@ class Person {
 }
 
 
-var fakePerson = {name:"Fake Shashi", age:34, weight:86}; //this is not a prototype of person.. well or is it.. since it has the two properties that a person has..?
+class Earthling extends Person {
+  whoAmI() {console.log("I am an Earthling")};
+}
+
+//structurally identical to Person but nominally different
+class Alien {
+  name: string;
+  age: number;
+
+  constructor(name:string,age:number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+
+var fakePerson = {name:"Fake Shashi", age:34, weight:86}; //  Object and Interface types are structural. Thus it is not treated nominally by flow and will not throw any errors as structurally same.
 var pet = {type:'Dog', name:'Panther'};
 var realPerson = new Person('Shashi',32);
+var alien = new Alien('ET', 2134);
+var earthling = new Person('Jake',34);
 
 printIntroduction(fakePerson);
 printIntroduction(realPerson);
 printIntroduction(pet);
+printIntroduction(alien); //this throws an error because Alien is a class and is nominally different. Flow treats classes as nominal types
+printIntroduction(earthling);
